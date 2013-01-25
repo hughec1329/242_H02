a0652a6de770c0dce06b85ec47a12c5387a80cbf
hughec1329 <hughcrockford@gmail.com>
2013-01-25 12:47:42 -0800

    combine positions into single pos vector, description of map into single names dimcar vector

--- H02.R
+++ H02.R
@@ -10,11 +10,10 @@ buildMap = function(p,pc=0.5,r=100,c=100){ 	# p - percent covered, pc = percent 
 	n 	= r * c * p
 	nred 	= n * pc
 	nblue 	= n * (1 - p)
-	map  	= data.frame(x = 1:c, y = 1:r)	# build map area - unnececcary.
-	map$dimx = c 
-	map$dimy = r	# use attributes, or list to store dimension as recquired?
-	posR 	= data.frame(x = sample(1:c,nred,replace = TRUE), y = sample(1:r,nred, replace = TRUE))
-	posB 	= data.frame(x = sample(1:c,nblue,replace = TRUE), y = sample(1:r,nblue,replace = TRUE))
+	dimcar 	= c(r,c,p,n,pc,nred,nblue)
+	nom=c("rows","column","pc_covered","n_cars","pc_red","n_red","n_blue")
+	names(dimcar) = nom	
+	pos 	= data.frame(col = sample(c("red","blue"),n,replace = TRUE),x = sample(1:c,n,replace = TRUE), y = sample(1:r,n, replace = TRUE))
 	cars = list("dim" = dimcar, "red" = posR, "blue" = posB)
 	#	class(cars) = Bmap
 	return(cars)
