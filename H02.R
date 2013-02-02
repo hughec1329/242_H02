98af23fa5964795026f2085fe263e16b8571b1ef
hughec1329 <hughcrockford@gmail.com>
2013-02-02 14:01:41 -0800

    simplified conflict resolution using matching string of position. trains still not moving

--- H02.R
+++ H02.R
@@ -3,73 +3,6 @@
 
 # build area and populate
 
-# this version can have cars in same position!!!
-
-buildMap = function(p,pc=0.5,r=100,c=100){ 	# p - percent covered, pc = percent red.
-	if( p>1 | p<0){
-		print("invalid p")
-	}
-	n 	= r * c * p
-	nred 	= n * pc
-	nblue 	= n * (1 - pc)
-	dimcar 	= c(r,c,p,n,pc,nred,nblue)
-	nom=c("rows","column","pc_covered","n_cars","pc_red","n_red","n_blue")
-	names(dimcar) = nom	
-	pos 	= data.frame(x = sample(1:c,n,replace = TRUE), y = sample(1:r,n, replace = TRUE), col = rep(c(1,2),c(nred,nblue)))
-	cars = list("dim" = dimcar, "pos" = pos)
-	#	class(cars) = Bmap
-	return(cars)
-}	
-
-m = buildMap(.2,.5,10,10)
-
-# put this inside plot.bml?
-
-grid = matrix(0,m$dim["rows"],m$dim["column"])
-grid[ as.matrix(m$pos[m$pos["col"] == 1 ,c("x","y") ])] = 1
-grid[ as.matrix(m$pos[m$pos["col"] == 2 ,c("x","y") ])] = 2
-image(grid,col=c("white","red","blue"))
-
-
-
-setClass("bml",representation(dim="numeric",pos="data.frame"))
-class(m) = "bml"
-
-plot(m$pos[[1]],m$pos[[2]], col = c("red","blue")[m$pos[[3]]],pch=15)
-
-plot.bml = function(x){
-
-}
-
-# try to get whole image scaled on size - 
-# dev.new and X11.options in plot call.
-# pixels/inch?
-
-# develop class and plot.method
-
-
-
-# move cars
-
-############################3
-# try as matrix
-###############################
-
-m = matrix(sample(0:1,10000,replace = TRUE),100,100)
-image(m,axes = FALSE)
-
-buildMap = function(p,pc=0.5,r=100,c=100){ 	# p - percent covered, pc = percent red.
-	n = r*c 	#total number spaces
-	ncars = p*n
-	cars = sample(1:n, ncars, replace = TRUE)	# where to put cars
-	map = rep(0,n)
-	map[cars] = sample(c(1,2),replace = TRUE)
-	m = matrix(map,r,c)
-	return(m)
-}
-	
-
-####### alt method to chose where cars go - cannot repeat same car.
 map = function(p,pc,r,c){
 	n = p * r * c
 	nred = n*pc
@@ -85,9 +18,6 @@ map = function(p,pc,r,c){
 	return(cars)
 }
 
-
-m = map(.2,.5,10,10)
-
 plot.bml = function(m){
 	grid = matrix(0,m$dim["rows"],m$dim["column"])
 	grid[ as.matrix(m$pos[m$pos["col"] == 1 ,c("x","y") ])] = 1
@@ -102,28 +32,64 @@ summary.bml = function(x){
 	print(list(dim = dim,n = n,p = p))
 }
 
-plot(m)
 
 move = function(x,t){
 	copy = x		# make working copy
 	u = x$pos	# throw away dims
 	new = u
 	if(t %% 2){	# odd, move blue up. move up then check for conflicts.
+		nmov = nrow(u[u$col ==1,])
 		newy = u[u$col==2,]["y"]+1
-		newy[newy>x$dim["rows"]]=1
+		newy[newy>x$dim["rows"]]=1	# if fallen off edge, put at start
 		new[new$col ==2,]["y"]=newy		# update pos?
 	}
 	# else move red right
 	if(!t %% 2){	# odd, move blue up. move up then check for conflicts.
+		nmov = nrow(u[u$col ==1,])
 		newx = u[u$col==1,]["x"]+1
 		newx[newx>x$dim["column"]]=1
 		new[new$col ==1,]["x"]=newx		# update pos?
 	}
-	# conflict not working.
-	conflict = duplicated(rbind(x$pos[,1:2],new[,1:2]))[(1+nrow(new)):(2*nrow(new))]
+	# conflict working but not if same color next door.
+	# conflict = duplicated(rbind(x$pos[,1:2],new[,1:2]))[(1+nrow(new)):(2*nrow(new))]
+	unewchar = paste(new$x,new$y,sep = "")
+	uchar = paste(u$x,u$y,sep = "")
+	conflict = unewchar %in% uchar		# and if not same color?
 	copy$pos[!conflict,1:2] = new[!conflict,1:2]
+	velocity = 1 - sum(copy$pos != new) / nmov	# depends which color moving, need to check while 
+	print(velocity)
 	return(copy)
 }
 
-mnew = move(m,1)
+m = map(.2,.5,10,10)
+plot(m)
 
+mnew = move(m,1)
+plot(mnew)
+
+mnew = move(m,2)
+plot(mnew)
+
+play = function(m,t) {
+	vhs = array(0,dim = c(m$dim[["n_cars"]],3,6))plot(mnew)
+	for(i in 1:t)
+
+
+
+ah$x %in% as.numeric(rownames(table(ah$x)))[table(ah$x)>1] # which are in same column. need intersection & with row=row+1 
+ah$y %in% (ah$y+1)
+
+# try m$posx == mnew$posx
+
+# also try thorwing pos and col into character, then match mnew to m.
+
+# can mark trains after buildmap - only need to do it once.
+## for(i in xcars){
+	train == TRUE
+	while(train == TRUE)
+		if(car1 = car2)
+			col = redTrain
+		else
+			train == FALSE
+}
+ sapply(1:nrow(m$pos),function(i) paste(m$pos[i,0],sep = ","))
